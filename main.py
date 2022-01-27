b40bf432b3bc442a210f4413010fc092005f6401
p-r-o-m-e <97275566+p-r-o-m-e@users.noreply.github.com>
2022-01-27 03:49:45 -0800

    finished help command

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from replit import db
 
 TOKEN = os.environ["DISCORD_TOKEN"]
 
-prefix = ":D"
+prefix = ":V"
 bot = discord.Client()
 print("running")
 
@@ -42,18 +42,30 @@ async def on_message(message):
             elif con == "ping":
                 await message.channel.send("pong!")
             elif con == "help":
-              e=emb(f"**For more info:** ``` {prefix} help [command] ```", f"**Add ``` {prefix} ``` before any command**")
+              e=emb(f"**For more info:** ` {prefix} help [command] `", f"**Add ` {prefix} ` before any command**")
               e.set_author(name="Commands", url="", icon_url="https://image.freepik.com/free-vector/blue-pencil-with-pixel-art-style_475147-504.jpg")
-              
-              space = "\a\n\a"
-              em = lambda a,b,c="" : e.add_field(name=a,value=f"``` {b} ```{c}", inline=False)
-           
-              em(f"{space}:bookmark: Profile commands :bookmark:", "start, profile, attributes, boosts, cv", space)
-              em(":beginner: Menu commands :beginner:", "bank, shop, jobs, education, special", space)
-              em(":gift: Rewards commands :gift:", "daily, votetrend, checkin, quiz", space)
-              em(":currency_exchange: Interaction commands :currency_exchange:", "give, phone", space)
-              em(":diamonds: Misc commands :diamonds:", "setprefix, msgdev")
+              # s=""
+              space = chr(173) + "\n" + chr(173)
 
+              def em(a, b, c=""):
+                st = ""
+                for s in b:
+                  st += f"`{s}`, " 
+                st= st[:-2] + st[-1:]
+
+                return e.add_field(name=a,value=f"{st} {c}", inline=False)
+
+              # em(f"{space}:bookmark: Profile commands :bookmark:", ("start", "profile", "attributes", "boosts", "events", "likes", "inventory"), space)
+              # em(":beginner: Menu commands :beginner:", ("bank", "shop", "jobs", "education", "health", "apartments", "relationship"), space)
+              # em(":gift: Rewards commands :gift:", ("daily", "weekly", "votetrend", "checkin", "redeem", "quiz"), space)
+              # em(":currency_exchange: Interaction commands :currency_exchange:", ("mail", "give", "phone"), space)
+              # em(":diamonds: Misc commands :diamonds:", ("action", "setprefix", "inv","msgdev"))
+
+              em(f"{space}:bookmark: Profile commands :bookmark:", ("start", "profile", "attributes", "boosts", "events", "likes", "inventory"))
+              em(f"{space}:beginner: Menu commands :beginner:", ("bank", "shop", "jobs", "education", "health", "apartments", "relationship"))
+              em(f"{space}:gift: Rewards commands :gift:", ("daily", "weekly", "votetrend", "checkin", "redeem", "quiz"))
+              em(f"{space}:currency_exchange: Interaction commands :currency_exchange:", ("mail", "give", "phone"))
+              em(f"{space}:diamonds: Misc commands :diamonds:", ("action", "setprefix", "inv","msgdev"))
               await message.channel.send(embed=e)
                 
                 
