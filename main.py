30e514592e4bb96ec0e39ce4247fb74f310ede14
p-r-o-m-e <97275566+p-r-o-m-e@users.noreply.github.com>
2022-01-28 04:16:01 -0800

    Add more to Start command

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import discord
 import asyncio
 from replit import db
+from datetime import date
+
 # from dotenv import load_dotenv
 # from ds import *
 
@@ -12,6 +14,15 @@ TOKEN = os.environ["DISCORD_TOKEN"]
 prefix = ":V"
 bot = discord.Client()
 print("running")
+
+#db functions
+
+def addDB(cat, item):
+  pass
+
+
+# -----------------------
+
 def emb(t,desc="", col = 0x3AABC2):
         embedVar = discord.Embed(title=t, description=desc, color=col)
         return embedVar
@@ -19,6 +30,8 @@ def emb(t,desc="", col = 0x3AABC2):
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    activity = discord.Game(name=f"Disco-Life  | {prefix} help", type=3)
+    await bot.change_presence(status=discord.Status.online, activity=activity)
 
 @bot.event
 async def on_message(message):
@@ -26,7 +39,13 @@ async def on_message(message):
         return
     else:
         if bot.user.mentioned_in(message):
-          await message.channel.send(f"You can use ``` {prefix} help ``` for info! {message.author.mention}")
+          try:
+              user = db["life"]["disco_users"][message.author.id]
+          except KeyError:
+            await message.channel.send(f"You're a new face! `{prefix}start` to play or `{prefix} help` for info. {message.author.mention}")
+          else:
+            await message.channel.send(f"You can use ``` {prefix} help ``` for info! {message.author.mention}")
+        
         con1 = message.content
         if con1.startswith(f"{prefix.lower()} ") or con1.startswith(f"{prefix} "):
             con = con1.split(" ", 1)
@@ -58,23 +77,29 @@ async def on_message(message):
             elif con == 'start':
               u = user = message.author
               try:
-                 user = db["life"]["disco_users"][user]
+                 user = db["life"]["disco_users"][user.id]
               except KeyError:
-                #  await message.channel.send(e)
-                 msg = await message.channel.send(f"Hi {user.mention}!\nMake sure you have read and accepted the rules (`{prefix} rules`).\nif Yes, react with :thumbsup: !")
+
+                 msg = await message.channel.send(f">>> [ boots up ]\n\nYou want to play Disco-Life! \nCheck out gameplayinfo,\nMake sure you have read and\naccepted the rules .\nThen react with :thumbsup: !\n\n`{prefix.lower()} gameplayinfo`, `{prefix.lower()} rules`")
                  await msg.add_reaction(emoji="\N{THUMBS UP SIGN}")
 
                    #  had an error for so long because of typing - ch(user, reaction) here :(
                  def ch(reaction, user): 
-                
                    return user == u and str(reaction) == "\N{THUMBS UP SIGN}" and reaction.message == msg
+                   
                  try:
-                  r = await bot.wait_for("reaction_add", timeout=15.0, check = ch)
+                  r = await bot.wait_for("reaction_add", timeout=25.0, check = ch)
+                  del r
                  except asyncio.exceptions.TimeoutError:
                     await message.channel.send("`TimeOutError`")
                  else:
-                    await message.channel.send(f"{r[0]}")
-                      
+                  del msg
+                  msg = f"Hello {message.author.name}.\nI work for Discorp inc. And you are a product of our project Disco-Life!\nNow we intend to observe how you live in Disco-Verse, i hope you are ready!"
+
+                  today = date.today().strftime("%d-%m-%Y")
+                  e =emb("[New] Text Message ", f"`{str(today)}`\n\n" + msg)
+                  e.set_author(name = "Agent Disco", url = "", icon_url = "http://clipart-library.com/image_gallery/n829721.jpg")
+                  await message.channel.send(embed=e)
 
             else:
                 await message. channel.send("`Err : Invalid command.`")  
